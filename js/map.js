4ad280f5c35437d074eb1a1fc0ac9007a51384f2
Kevin Yeh <kevinyeah@utexas.edu>
2015-07-22 17:02:32 -0400

    Fix cluster buttons

--- js/map.js
+++ js/map.js
@@ -1,3 +1,7 @@
+var clusterSQLpre = "WITH meta AS (    SELECT greatest(!pixel_width!,!pixel_height!) as psz, ext, ST_XMin(ext) xmin, ST_YMin(ext) ymin FROM (SELECT !bbox! as ext) a  ),  filtered_table AS (    SELECT t.* FROM (";
+
+var clusterSQLpost = ") t, meta m WHERE t.the_geom_webmercator && m.ext  ), bucketA_snap AS (SELECT ST_SnapToGrid(f.the_geom_webmercator, 0, 0, m.psz * 48, m.psz * 48) the_geom_webmercator, count(*) as points_count, 1 as cartodb_id, array_agg(f.cartodb_id) AS id_list  FROM filtered_table f, meta m  GROUP BY ST_SnapToGrid(f.the_geom_webmercator, 0, 0, m.psz * 48, m.psz * 48), m.xmin, m.ymin), bucketA  AS (SELECT * FROM bucketA_snap WHERE points_count >  48 * 1 ) , bucketB_snap AS (SELECT ST_SnapToGrid(f.the_geom_webmercator, 0, 0, m.psz * 0.75 * 48, m.psz * 0.75 * 48) the_geom_webmercator, count(*) as points_count, 1 as cartodb_id, array_agg(f.cartodb_id) AS id_list  FROM filtered_table f, meta m  WHERE cartodb_id NOT IN (select unnest(id_list) FROM bucketA)  GROUP BY ST_SnapToGrid(f.the_geom_webmercator, 0, 0, m.psz * 0.75 * 48, m.psz * 0.75 * 48), m.xmin, m.ymin), bucketB  AS (SELECT * FROM bucketB_snap WHERE points_count >  48 * 0.75 ) , bucketC_snap AS (SELECT ST_SnapToGrid(f.the_geom_webmercator, 0, 0, m.psz * 0.5 * 48, m.psz * 0.5 * 48) the_geom_webmercator, count(*) as points_count, 1 as cartodb_id, array_agg(f.cartodb_id) AS id_list  FROM filtered_table f, meta m  WHERE cartodb_id NOT IN (select unnest(id_list) FROM bucketA)  AND cartodb_id NOT IN (select unnest(id_list) FROM bucketB)  GROUP BY ST_SnapToGrid(f.the_geom_webmercator, 0, 0, m.psz * 0.5 * 48, m.psz * 0.5 * 48), m.xmin, m.ymin), bucketC  AS (SELECT * FROM bucketC_snap WHERE points_count >  GREATEST(48 * 0.1, 2)  )  SELECT the_geom_webmercator, 1 points_count, cartodb_id, ARRAY[cartodb_id] as id_list, 'origin' as src, cartodb_id::text cdb_list FROM filtered_table WHERE cartodb_id NOT IN (select unnest(id_list) FROM bucketA) AND cartodb_id NOT IN (select unnest(id_list) FROM bucketB) AND cartodb_id NOT IN (select unnest(id_list) FROM bucketC)  UNION ALL SELECT *, 'bucketA' as src, array_to_string(id_list, ',') cdb_list FROM bucketA UNION ALL SELECT *, 'bucketB' as src, array_to_string(id_list, ',') cdb_list FROM bucketB UNION ALL SELECT *, 'bucketC' as src, array_to_string(id_list, ',') cdb_list FROM bucketC";
+
 var sublayers = [];
 
 var LayerActions = {
@@ -19,7 +23,7 @@ cartodb.createVis('map', 'https://kyeah.cartodb.com/api/v2/viz/6f8589b6-2f5a-11e
     .on('done', function(map, layers) {
         // change the query for the first layer
         var sublayer = layers[1].getSubLayer(0);
-        console.log(sublayer);
+        sublayer.setSQL(clusterSQLpre + "SELECT * FROM hlcp_2" + clusterSQLpost);
         sublayers.push(sublayer);
         /*
         map.overlayMapTypes.setAt(1, layers[1]);
@@ -52,8 +56,7 @@ $('.button').click(function() {
     $(this).addClass('selected');
     var checked = $('.button.selected').map(function() { return "'%" + this.id + "'" }).get();
     console.log(checked);
-    console.log("SELECT * FROM hlcp_2 WHERE date LIKE (" + checked.join() + ");");
-    sublayers[0].setSQL("SELECT * FROM hlcp_2 WHERE date LIKE (" + checked.join() + ");");
+    sublayers[0].setSQL(clusterSQLpre + "SELECT * FROM hlcp_2 WHERE date LIKE (" + checked.join() + ")" + clusterSQLpost);
     return true;
 });
 
