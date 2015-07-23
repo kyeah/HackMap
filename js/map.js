b2c066b9242d9db65822e5cd1b14f0e0d20ea9ad
Kevin Yeh <kevinyeah@utexas.edu>
2015-07-22 21:16:05 -0700

    Fix year selector for all layers
    Uses terrible hardcoding im sorry :(

--- js/map.js
+++ js/map.js
@@ -3,51 +3,48 @@ var clusterSQLpre = "WITH meta AS (    SELECT greatest(!pixel_width!,!pixel_heig
 var clusterSQLpost = ") t, meta m WHERE t.the_geom_webmercator && m.ext  ), bucketA_snap AS (SELECT ST_SnapToGrid(f.the_geom_webmercator, 0, 0, m.psz * 48, m.psz * 48) the_geom_webmercator, count(*) as points_count, 1 as cartodb_id, array_agg(f.cartodb_id) AS id_list  FROM filtered_table f, meta m  GROUP BY ST_SnapToGrid(f.the_geom_webmercator, 0, 0, m.psz * 48, m.psz * 48), m.xmin, m.ymin), bucketA  AS (SELECT * FROM bucketA_snap WHERE points_count >  48 * 1 ) , bucketB_snap AS (SELECT ST_SnapToGrid(f.the_geom_webmercator, 0, 0, m.psz * 0.75 * 48, m.psz * 0.75 * 48) the_geom_webmercator, count(*) as points_count, 1 as cartodb_id, array_agg(f.cartodb_id) AS id_list  FROM filtered_table f, meta m  WHERE cartodb_id NOT IN (select unnest(id_list) FROM bucketA)  GROUP BY ST_SnapToGrid(f.the_geom_webmercator, 0, 0, m.psz * 0.75 * 48, m.psz * 0.75 * 48), m.xmin, m.ymin), bucketB  AS (SELECT * FROM bucketB_snap WHERE points_count >  48 * 0.75 ) , bucketC_snap AS (SELECT ST_SnapToGrid(f.the_geom_webmercator, 0, 0, m.psz * 0.5 * 48, m.psz * 0.5 * 48) the_geom_webmercator, count(*) as points_count, 1 as cartodb_id, array_agg(f.cartodb_id) AS id_list  FROM filtered_table f, meta m  WHERE cartodb_id NOT IN (select unnest(id_list) FROM bucketA)  AND cartodb_id NOT IN (select unnest(id_list) FROM bucketB)  GROUP BY ST_SnapToGrid(f.the_geom_webmercator, 0, 0, m.psz * 0.5 * 48, m.psz * 0.5 * 48), m.xmin, m.ymin), bucketC  AS (SELECT * FROM bucketC_snap WHERE points_count >  GREATEST(48 * 0.1, 2)  )  SELECT the_geom_webmercator, 1 points_count, cartodb_id, ARRAY[cartodb_id] as id_list, 'origin' as src, cartodb_id::text cdb_list FROM filtered_table WHERE cartodb_id NOT IN (select unnest(id_list) FROM bucketA) AND cartodb_id NOT IN (select unnest(id_list) FROM bucketB) AND cartodb_id NOT IN (select unnest(id_list) FROM bucketC)  UNION ALL SELECT *, 'bucketA' as src, array_to_string(id_list, ',') cdb_list FROM bucketA UNION ALL SELECT *, 'bucketB' as src, array_to_string(id_list, ',') cdb_list FROM bucketB UNION ALL SELECT *, 'bucketC' as src, array_to_string(id_list, ',') cdb_list FROM bucketC";
 
 var sublayers = [];
-
-var LayerActions = {
-    2011: function(){
-        sublayers[0].setSQL("SELECT * FROM ne_10m_populated_places_simple");
-        return true;
-    },
-    2012: function(){
-        sublayers[0].setSQL("SELECT * FROM ne_10m_populated_places_simple WHERE featurecla = 'Admin-0 capital'");
-        return true;
-    },
-    megacities: function(){
-        sublayers[0].setSQL("SELECT * FROM ne_10m_populated_places_simple WHERE megacity = 1");
-        return true;
-    }
-}
+var mylayers = [];
 
 cartodb.createVis('map', 'https://kyeah.cartodb.com/api/v2/viz/6f8589b6-2f5a-11e5-b4e8-0e9d821ea90d/viz.json')
     .on('done', function(map, layers) {
-        // change the query for the first layer
-        var sublayer = layers[1].getSubLayer(0);
-        sublayer.setSQL(clusterSQLpre + "SELECT * FROM hlcp_2" + clusterSQLpost);
-        sublayers.push(sublayer);
+        
+        // Sorry...hardcoded for now.
+        var cluster_sublayer = layers[1].getSubLayer(0);
+        var info_sublayer = layers[1].getSubLayer(1);
+        var time_sublayer = layers[2];
+
+        sublayers.push(cluster_sublayer);
+        sublayers.push(info_sublayer);
+        sublayers.push(time_sublayer);
+
+        allSQL = "SELECT * from hlcp_2";
+        cluster_sublayer.setSQL(clusterSQLpre + allSQL + clusterSQLpost);
+        info_sublayer.setSQL(allSQL);
+        time_sublayer.setSQL(allSQL);
+
         /*
-        map.overlayMapTypes.setAt(1, layers[1]);
-        var sublayer = layer[1].getSubLayer(0);
-        sublayer.setCartoCSS(systemcartoCSS);
+          map.overlayMapTypes.setAt(1, layers[1]);
+          var sublayer = layer[1].getSubLayer(0);
+          sublayer.setCartoCSS(systemcartoCSS);
 
-        layers.SystemLyr = sublayer
-        var infowindow = sublayer.infowindow
+          layers.SystemLyr = sublayer
+          var infowindow = sublayer.infowindow
 
-        infowindow.set('template', function(data) {
+          infowindow.set('template', function(data) {
 
-            var clickPosLatLng = this.model.get('latlng');
-            var fields = this.model.get('content').fields;
+          var clickPosLatLng = this.model.get('latlng');
+          var fields = this.model.get('content').fields;
 
-            if (fields && fields[0].type !== 'loading') {
+          if (fields && fields[0].type !== 'loading') {
 
-                var obj = _.find(fields, function(obj) {
-                    return obj.title == 'kml_key'
-                }).value
+          var obj = _.find(fields, function(obj) {
+          return obj.title == 'kml_key'
+          }).value
 
-                callinfowindow(clickPosLatLng, obj)
+          callinfowindow(clickPosLatLng, obj)
 
-            }
-        }); // end infowindow set
+          }
+          }); // end infowindow set
         */
     });
 
@@ -55,8 +52,11 @@ $('.button').click(function() {
     $('.button').removeClass('selected');
     $(this).addClass('selected');
     var checked = $('.button.selected').map(function() { return "'%" + this.id + "'" }).get();
-    console.log(checked);
-    sublayers[0].setSQL(clusterSQLpre + "SELECT * FROM hlcp_2 WHERE date LIKE (" + checked.join() + ")" + clusterSQLpost);
+
+    var sql = "SELECT * FROM hlcp_2 WHERE date LIKE (" + checked.join() + ")";
+    sublayers[0].setSQL(clusterSQLpre + sql + clusterSQLpost);
+    sublayers[1].setSQL(sql);
+    sublayers[2].setSQL(sql);
+
     return true;
 });
-
